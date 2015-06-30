79c7d54271608bbcbeb6e864dddc4b0288687ec4
hmc0316 <hmc0316@gmail.com>
2015-06-30 17:56:37 +0800

    change some error

--- getip.php
+++ getip.php
@@ -11,10 +11,10 @@ $redis = new Redis();
 $redis_ip = $GLOBALS['memcache_ip'];        
 $redis->connect($redis_ip);
 $thisiplong = sprintf("%u",ip2long(user_ip()));
-$res = $redis->zRangeByScore('etip:index', $thisiplong , '+inf', array('limit' => array(0, 1)));
+$res = $redis->zRangeByScore('key:index', $thisiplong , '+inf', array('limit' => array(0, 1)));
 $redis_key =  "etip:".$res[0];
 $redis_arr = $redis->hmGet($redis_key,array("code","country"));
 
-setcookie ('et_client_country',$redis_arr['code'],time()+3600*24*7,"/","ettoday.net" );//do 都有
-$GLOBALS['et_client_country'] = $redis_arr['code'];    
+setcookie ('client_country',$redis_arr['code'],time()+3600*24*7,"/","domain.net" );//do 都有
+$GLOBALS['client_country'] = $redis_arr['code'];    
 $redis->close();
